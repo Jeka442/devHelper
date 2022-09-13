4b6bb57d49430237ad1ef94d58300e608c829380
Jeka <jeka.gudilov@allgreen.app>
2022-09-13 19:59:09 +0300

    damn it

--- Popup/popup.js
+++ Popup/popup.js
@@ -1,7 +1,7 @@
 async function onLoad() {
-    const lastPage = await chrome.storage.local.get("lastPage");
+    const { lastPage } = await chrome.storage.local.get("lastPage");
     if (lastPage) {
-        window.location.href = `./${lastPage.lastPage}/index.html`;
+        window.location.href = `./${lastPage}/index.html`;
     } else {
         window.location.href = "./home/index.html"
     }
