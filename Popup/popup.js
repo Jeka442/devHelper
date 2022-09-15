abc42ce1d3007a6f23c8dfa8e364d9e9ce07745b
Jeka <jeka.gudilov@allgreen.app>
2022-09-16 01:37:53 +0300

    ok

--- Popup/popup.js
+++ Popup/popup.js
@@ -1,9 +1,14 @@
 async function onLoad() {
-    const { lastPage } = await chrome.storage.local.get("lastPage");
-    if (lastPage) {
-        window.location.href = `./${lastPage}/index.html`;
-    } else {
-        window.location.href = "./home/index.html"
+    try {
+
+        const { lastPage } = await chrome.storage.local.get("lastPage");
+        if (lastPage) {
+            window.location.href = `./${lastPage}/index.html`;
+        } else {
+            window.location.href = "./home/index.html";
+        }
+    } catch (e) {
+        window.localStorage.href = "./home/index.html";
     }
 }
 onLoad();
